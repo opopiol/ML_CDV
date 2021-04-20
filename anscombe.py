1341fa97a6e4705d9756bae60a277bd340857eb2
opopiol <opopiol@edu.cdv.pl>
2021-04-20 12:17:35 +0200

    Commit

--- anscombe.py
+++ anscombe.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 x = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
 y1 = [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
@@ -17,14 +18,19 @@ df_dict = {
 
 anscombe_df = pd.DataFrame(df_dict, index=x)
 
-print(anscombe_df.mean().round(2))
-print(anscombe_df.std(ddof=0).round(2))
-print(anscombe_df.reset_index().corr().round(2))
-print(anscombe_df.var().round(1))
+results = {'Mean': [anscombe_df.mean().round(2)],
+    'Standard deviation': [anscombe_df.std(ddof=0).round(2)],
+    'Correlation': [anscombe_df.reset_index().corr().round(2)],
+    'Variation': [anscombe_df.var().round(1)]}
+results_csv = pd.DataFrame(results)
+
+dir=os.mkdir('Anscombes_quartet')
+results_csv.to_csv(f'Anscombes_quartet/results.csv', index=False, header=True, sep=',')
 
 scatter_plot_df = anscombe_df.reset_index()
 
 fig, axs = plt.subplots(2,2, sharex=True, sharey=True, figsize=(6, 6))
+fig.suptitle('Anscombes quartet', fontsize=16)
 
 axs[0, 0].set(xlim=(0, 20), ylim=(2, 14))
 axs[0, 0].set(xticks=(0, 10, 20), yticks=(0, 4, 8, 12))
@@ -34,4 +40,4 @@ axs[0, 1].scatter(x, y2)
 axs[1, 0].scatter(x, y3)
 axs[1, 1].scatter(x4, y4)
 
-plt.show()
+plt.savefig(f'Anscombes_quartet/plot.png')
