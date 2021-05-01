7fe2cef94d08b1589e091d5748249929b772f1ae
opopiol <opopiol@edu.cdv.pl>
2021-05-01 17:49:00 +0200

    Commit

--- anscombe.py
+++ anscombe.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import seaborn as sns
 
 x = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
 y1 = [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
@@ -9,23 +10,24 @@ y3 = [7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73]
 x4 = [8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8]
 y4 = [6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.50, 5.56, 7.91, 6.89]
 
-df_dict = {
-    'I': y1,
-    'II': y2,
-    'III': y3,
-    'IV': y4
-}
-
-anscombe_df = pd.DataFrame(df_dict, index=x)
-
-results = {'Mean': [anscombe_df.mean().round(2)],
-    'Standard deviation': [anscombe_df.std(ddof=0).round(2)],
-    'Correlation': [anscombe_df.reset_index().corr().round(2)],
-    'Variation': [anscombe_df.var().round(1)]}
-results_csv = pd.DataFrame(results)
-
 dir=os.mkdir('Anscombes_quartet')
-results_csv.to_csv(f'Anscombes_quartet/results.csv', index=False, header=True, sep=',')
+
+anscombe_df = sns.load_dataset("anscombe")
+
+mean = anscombe_df.groupby("dataset").mean().round(2)
+mean_csv = pd.DataFrame(mean)
+std = anscombe_df.groupby("dataset").std(ddof=0).round(2)
+std_csv = pd.DataFrame(std)
+corr = anscombe_df.groupby("dataset").corr().round(2)
+corr_csv = pd.DataFrame(corr)
+var = anscombe_df.groupby("dataset").var().round(1)
+var_csv = pd.DataFrame(var)
+
+
+results=pd.concat([mean_csv, std_csv, corr_csv, var_csv])
+print(results)
+results.to_csv(f'Anscombes_quartet/results.csv', index=False, header=True, sep=';', float_format='%.2f')
+
 
 scatter_plot_df = anscombe_df.reset_index()
 
@@ -41,3 +43,4 @@ axs[1, 0].scatter(x, y3)
 axs[1, 1].scatter(x4, y4)
 
 plt.savefig(f'Anscombes_quartet/plot.png')
+
