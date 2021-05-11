310a2dc135c74b40136751e9a6655cf3a2ab1e42
opopiol <opopiol@edu.cdv.pl>
2021-05-11 15:07:01 +0200

    Adding docstrings

--- anscombe.py
+++ anscombe.py
@@ -1,16 +1,23 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import os
 import seaborn as sns
 
 
 def get_anscombe_data():
+    """This function loads Anscombe's quartet data from seaborn's library
+    :return: anscombe_df: Data frame with Anscombe's quartet values
+    """
+
     anscombe_df = sns.load_dataset("anscombe")
 
     return anscombe_df
 
 
 def get_anscombe_results(anscombe_df):
+    """This function calculates mean, standard deviation, variance and correlation
+    :param anscombe_df: File name of input data
+    :return: results: dataframe's calculations
+    """
 
     mean = anscombe_df.groupby("dataset").mean().round(2)
     mean_csv = pd.DataFrame(mean)
@@ -27,6 +34,11 @@ def get_anscombe_results(anscombe_df):
 
 
 def get_anscombe_plot(anscombe_df):
+    """This function creates plot for each dataset
+    :param anscombe_df: File name of input data
+    :return: ansc_plot: Show the results of a linear regression within each dataset
+    """
+
     ansc_plot = sns.lmplot(x="x", y="y", col="dataset", hue="dataset",
                data=anscombe_df, col_wrap=2, ci=None, palette="muted",
                height=4, scatter_kws={"s": 50, "alpha": 1})
@@ -35,6 +47,8 @@ def get_anscombe_plot(anscombe_df):
 
 
 def main():
+    """This function creates directory and saves results in csv and plot"""
+
     anscombe_dir = os.mkdir('anscombes_quartet')
 
     anscombe_df = get_anscombe_data()
@@ -44,7 +58,6 @@ def main():
     results.to_csv(os.path.join('anscombes_quartet', 'results.csv'))
     ansc_plot.savefig(os.path.join('anscombes_quartet', 'plot.png'))
 
+
 if __name__ == "__main__":
     main()
-
-
